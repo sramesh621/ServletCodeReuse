a9df858b890b1402f8bbe881b75a757e5d69f775
sramesh621 <ramesh.srigiriraju@gmail.com>
2026-08-02 16:46:57 -0400

    Create FormServletA class for handling HTTP requests

---
+++ src/com/lalithsoftware/web/FormServletA.java
@@ -0,0 +1,18 @@
+package com.lalithsoftware.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSsevletResponse;
+
+public class FormServletA extends HttpServlet
+  {
+    public class FormServletA()
+    {
+    }
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+    {
+      
+    }
+  }
