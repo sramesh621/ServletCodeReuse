7eedabad1a25a16af5869f15a31c933cac076a9a
sramesh621 <ramesh.srigiriraju@gmail.com>
2026-08-31 06:43:11 -0400

    Add Javadoc for HttpServletRequestCheckResult interface
    Added documentation comment for the interface.

--- src/com/lalithsoftware/web/HttpServletRequestCheckResult.java
+++ src/com/lalithsoftware/web/HttpServletRequestCheckResult.java
@@ -1,5 +1,9 @@
 package com.lalithsoftware.web;
 
+/**
+ * Interface for implementing the code for checking for null HTTP request
+ * objects.
+ */
 public interface HttpServletRequestCheckResult  {
-      public void executeCheckResultCode();
+    public void executeCheckResultCode();
 }
