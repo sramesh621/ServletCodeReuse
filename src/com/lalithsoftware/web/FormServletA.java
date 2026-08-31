2cbedf6932b2e13515f6e17b1a0e92e0a7b5e124
sramesh621 <ramesh.srigiriraju@gmail.com>
2026-08-31 15:59:32 -0400

    Fix constructor and import issues in FormServletA

--- src/com/lalithsoftware/web/FormServletA.java
+++ src/com/lalithsoftware/web/FormServletA.java
@@ -5,14 +5,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSsevletResponse;
 
 public class FormServletA extends HttpServlet
-  {
+{
+
+    HttpServletRequestCheckerFormA formARequestChecker = null;
+  
     public class FormServletA()
     {
+        formARequestChecker = new HttpServetRequestCheckerFormA();
     }
 
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
     {
-      
+        HttpServletRequestCheckResult servletRequestCheckResult =
+            formARequestChecker.checkHTTPRequest(request);
     }
-  }
+}
