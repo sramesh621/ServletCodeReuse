171f197345765f36dacd6f111e999f0bdf58a9ac
sramesh621 <ramesh.srigiriraju@gmail.com>
2026-08-31 06:31:28 -0400

    Add getters and setters for request and response

--- src/com/lalithsoftware/web/HttpServletRequestCheckNonNullRequest.java
+++ src/com/lalithsoftware/web/HttpServletRequestCheckNonNullRequest.java
@@ -11,10 +11,36 @@ public class HttpServletRequestCheckNonNullResult implements HttpServletRequestC
     public HttpServletRequestCheckNonNullResult()  {
 
     }
-  
+
+    public HttpServletRequest getHttpServletRequest()    {
+            return httpServletRequest;
+    }
+
+    public void setHttpServletRequest(HttpServletRequest newRequest)    {
+            httpServletRequest = newRequest;
+    }
+
+    public HttpServletResponse getHttpServletResponse()    {
+            return httpServletResponse;
+    }
+
+    public void setHttpServletResponse(HttpServletResponse newResponse)    {
+            httpServletResponse = newResponse;
+    }
+
+    public ServletContext getServletContext()    {
+            return servletContext;
+    }
+
+    public void setServletContext(ServletContext newServletContext)    {
+            servletContext = newServletContext;
+    }
+    
     public void executeRequestResultCode()  {
-        RequestDispatcher dispatcher =
-            getServletContext().getRequestDispatcher("/servlet/ShowSupplies");
+        HttpServletRequest httpReqeust = getHttpServletRequest();
+        HttpServletResponse httpResponse = getHttpServletResponse();
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher dispatcher = servletContext.getRequestDispatcher("/servlet/ShowSupplies");
         dispatcher.include(request, response);
     }
 }
