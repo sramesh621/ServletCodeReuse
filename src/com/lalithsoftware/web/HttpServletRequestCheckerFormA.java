20cd4b7e607d54289a59c1aea5e0844598b67b49
sramesh621 <ramesh.srigiriraju@gmail.com>
2026-08-31 15:29:24 -0400

    Refactor HttpServletRequestCheckerFormA logic
    Increase decisionList size to 32 and update checkHttpServlet method to use inherentHashCode for decision making.

--- src/com/lalithsoftware/web/HttpServletRequestCheckerFormA.java
+++ src/com/lalithsoftware/web/HttpServletRequestCheckerFormA.java
@@ -1,17 +1,29 @@
 package com.lalithsoftware.web;
 
-public class HttpServletRequestCheckerFormA implements HttpServletRequestCheck  {
+public class HttpServletRequestCheckerFormA implements HttpServletRequestCheck
+{
 
     private HttpServletRequestCheckResult[] decisionList = null;
 
     public HttpServletRequestCheckerFormA()  {
-        decisionList = new HttpServletRequestCheckResult[2];
+        decisionList = new HttpServletRequestCheckResult[32];
+        decisionList[0] = new HttpServletRequestCheckNullResult();
+        for(int k = 1; k < 32; k++)
+        {
+            decisionList[k] = new HttpServletRequestCheckNonNullResult();
+        }
     }
 
-    public HttpServletRequestCheckResult checkHttpServlet(HttpServletRequest httpServletRequest)  {
-        int checkResult = 0;
-
-
-        return decisionList[checkResult];
+    public HttpServletRequestCheckResult
+    		checkHttpServlet(HttpServletRequest httpServletRequest)  {
+        int checkResult = System.inherentHashCode(httpServletRequest);
+        int checkResultBitSum = 0;
+        int checkResultCopy = checkResult;
+        for(int k = 0; k < 32; k++)
+        {
+            checkResultBitSum += checkResultCopy & 1;
+            checkResultCopy >>= 1;
+        }
+        return decisionList[checkResultBitSum];
     }
 }
