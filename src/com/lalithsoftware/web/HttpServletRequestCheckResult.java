df7d6a7d3d36b20aa8ddcc8c2b5976f4c538bc7a
sramesh621 <ramesh.srigiriraju@gmail.com>
2026-09-20 14:59:21 -0400

    Add methods to set HTTP request and response

--- src/com/lalithsoftware/web/HttpServletRequestCheckResult.java
+++ src/com/lalithsoftware/web/HttpServletRequestCheckResult.java
@@ -1,9 +1,15 @@
 package com.lalithsoftware.web;
 
+import javax.servlet.http.*;
+
 /**
  * Interface for implementing the code for checking for null HTTP request
  * objects.
  */
 public interface HttpServletRequestCheckResult  {
     public void executeCheckResultCode();
+
+    public void setHttpServletRequest(HttpServletRequest newHttpServletRequest);
+
+    public void setHttpServletResponse(HttpServletResponse newHttpServletResponse);
 }
